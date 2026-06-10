ef023d7f35abd852941e5b737c160a0a7fcd036a
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-10 14:06:55 +0300

    feat: Exercise 8 - Easy/ count 1 to 10

---
+++ src/Java_exercise_Easy/conditional_structure/Ex08_CountOneToTen.java
@@ -0,0 +1,9 @@
+package Java_exercise_Easy.conditional_structure;
+
+public class Ex08_CountOneToTen {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
