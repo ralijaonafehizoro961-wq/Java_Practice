39059a94a7306926ab92acd677d004f724cdf947
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-10 13:58:50 +0300

    feat : exercise 7 - Easy/even or odd

---
+++ src/Java_exercise_Easy/conditional_structure/Exo7_EvenOrOdd.java
@@ -0,0 +1,17 @@
+package Java_exercise_Easy.conditional_structure;
+
+import java.util.Scanner;
+
+public class Exo7_EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number ;
+        number = sc.nextInt();
+        if (number % 2 == 0){
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+    }
+}
