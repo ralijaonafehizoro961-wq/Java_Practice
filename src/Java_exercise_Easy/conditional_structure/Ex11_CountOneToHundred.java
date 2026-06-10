0a862ddd1305b2236cf8617fefaab013d9a673b4
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-10 14:42:28 +0300

    feat: Exercise 11 - Easy/ Count 1 to 100 with while

---
+++ src/Java_exercise_Easy/conditional_structure/Ex11_CountOneToHundred.java
@@ -0,0 +1,11 @@
+package Java_exercise_Easy.conditional_structure;
+
+public class Ex11_CountOneToHundred {
+    public static void main(String[] args) {
+        int i = 1;
+        while ( i <= 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
