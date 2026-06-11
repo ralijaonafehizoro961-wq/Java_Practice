dccb156753d4bb79c1394a74829332ceca854f66
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-11 13:23:23 +0300

    feat: exercice 13 - Easy/ display array

---
+++ src/Java_exercise_Easy/array_list/Ex13_DisplayArray.java
@@ -0,0 +1,10 @@
+package Java_exercise_Easy.array_list;
+
+public class Ex13_DisplayArray {
+    public static void main(String[] args) {
+        int[] tab = {1,2,3,4,5};
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+    }
+}
