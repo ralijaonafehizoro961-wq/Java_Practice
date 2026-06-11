9c737d45aae681b63b6156bba502256970f0ef9a
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-11 14:00:48 +0300

    feat: exercise 17 - Easy/ String length

---
+++ src/Java_exercise_Easy/array_list/Ex17_StringLength.java
@@ -0,0 +1,9 @@
+package Java_exercise_Easy.array_list;
+
+public class Ex17_StringLength {
+    public static void main(String[] args) {
+        String name = "Charlie";
+        int count = name.length();
+        System.out.println("count = " + count);
+    }
+}
