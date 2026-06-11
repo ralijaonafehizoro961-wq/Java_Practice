67123d1598048cdf45b94e3421c35312f78e0554
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-11 13:32:59 +0300

    feat:Exercise 14 - Easy/fill array

---
+++ src/Java_exercise_Easy/array_list/Ex14_FillArray.java
@@ -0,0 +1,10 @@
+package Java_exercise_Easy.array_list;
+
+public class Ex14_FillArray {
+    public static void main(String[] args) {
+        int[] tab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i : tab){
+            System.out.println(i);
+        }
+    }
+}
