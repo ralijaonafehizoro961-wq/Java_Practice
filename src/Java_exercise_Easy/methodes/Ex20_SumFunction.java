440f92b25c976275179cf91f7797ea1a4d3f00c0
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-12 12:56:40 +0300

    feat: exercise 20 - easy / Sum function

---
+++ src/Java_exercise_Easy/methodes/Ex20_SumFunction.java
@@ -0,0 +1,11 @@
+package Java_exercise_Easy.methodes;
+
+public class Ex20_SumFunction {
+    public static int sum(int a, int b){
+        return a + b ;
+    }
+    public static void main(String[] args) {
+        int sum = sum(5,10);
+        System.out.println("Sum : "+ sum);
+    }
+}
